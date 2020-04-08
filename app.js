4f7e2b220818377d302aa39d9c8aa87841e101eb
Poorvi Gupta <poorvi.gupta@zensar.com>
2020-04-08 12:44:07 +0530

    Create calculator object

---
+++ app.js
@@ -0,0 +1,15 @@
+const calculator={
+add:(a,b)=>{
+    return a+b;
+},
+subtract:(a,b)=>{
+    return a+b;
+},
+multiply:(a,b)=>{
+    return a+b;
+},
+divide:(a,b)=>{
+    return a+b;
+}
+console.log('Addition: ${calculator.add(10,20)}')
+}
