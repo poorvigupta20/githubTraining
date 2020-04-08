48959234e5c60250e7517f9359cd02a0d52e650c
poorvigupta20 <63336242+poorvigupta20@users.noreply.github.com>
2020-04-08 12:49:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ add:(a,b)=>{
     return a+b;
 },
 subtract:(a,b)=>{
-    return a+b;
+    return a-b;
 },
 multiply:(a,b)=>{
-    return a+b;
+    return a*b;
 },
 divide:(a,b)=>{
-    return a+b;
+    return a/b;
 }
 console.log('Addition: ${calculator.add(10,20)}')
+console.log('Subtraction: ${calculator.subtract(10,20)}')
+console.log('Multiplication: ${calculator.multiply(10,20)}')
+console.log('Division: ${calculator.divide(10,20)}')
 }
